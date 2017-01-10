a81f0c67155743c8654c2fde75b0881167d2f31e
pirklbauer31 <Kevin1995>
2017-01-10 21:47:08 +0100

    Worked on CreateEntryActivity

--- app/src/main/java/bandm8s/hagenberg/fh/bandm8s/CreateEntryActivity.java
+++ app/src/main/java/bandm8s/hagenberg/fh/bandm8s/CreateEntryActivity.java
@@ -4,13 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateEntryActivity extends AppCompatActivity {
+
+    //GUI
+    private Spinner mGenres;
+    private Spinner mSkill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_entry);
+
+        mGenres = (Spinner) findViewById(R.id.spinnerGenre);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> genreAdapter = ArrayAdapter.createFromResource(this,
+                R.array.genres_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        genreAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        mGenres.setAdapter(genreAdapter);
+
+        mSkill = (Spinner) findViewById(R.id.spinnerSkill);
+        ArrayAdapter<CharSequence> skillAdapter = ArrayAdapter.createFromResource(this,
+                R.array.skillLevel_array, android.R.layout.simple_spinner_item);
+        skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSkill.setAdapter(skillAdapter);
     }
 
     @Override
