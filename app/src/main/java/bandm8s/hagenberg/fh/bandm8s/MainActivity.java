b6e38a4af3784c72cd285739cca73db81cf22d0a
pirklbauer31 <Kevin1995>
2017-01-10 12:54:28 +0100

    Added CreateEntry Activity

--- app/src/main/java/bandm8s/hagenberg/fh/bandm8s/MainActivity.java
+++ app/src/main/java/bandm8s/hagenberg/fh/bandm8s/MainActivity.java
@@ -1,6 +1,7 @@
 package bandm8s.hagenberg.fh.bandm8s;
 
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -38,8 +39,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(getApplicationContext(), CreateEntryActivity.class);
+                startActivity(i);
             }
         });
 
