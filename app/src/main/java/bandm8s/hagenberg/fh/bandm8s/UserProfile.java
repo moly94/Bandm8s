7b420deb5f0fa3d6854858b64c65ca896d55cd57
Felpower <s1510237003@students.fh-hagenebrg.at>
2017-01-11 21:33:07 +0100

    Profile is now editable, but it won't save the edited things yet

--- app/src/main/java/bandm8s/hagenberg/fh/bandm8s/UserProfile.java
+++ app/src/main/java/bandm8s/hagenberg/fh/bandm8s/UserProfile.java
@@ -1,17 +1,93 @@
 package bandm8s.hagenberg.fh.bandm8s;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
-/**
- * Created by Felpower on 09.01.2017.
- */
+import static bandm8s.hagenberg.fh.bandm8s.R.id.imageView;
+
 
 public class UserProfile extends AppCompatActivity {
+
+    private Spinner mGenres;
+    private Spinner mSkill;
+    private ImageButton changeProfilePicture;
+    private ImageView profilePicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
+
+
+        mGenres = (Spinner) findViewById(R.id.spinnerGenre);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> genreAdapter = ArrayAdapter.createFromResource(this,
+                R.array.genres_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        genreAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        mGenres.setAdapter(genreAdapter);
+
+        mSkill = (Spinner) findViewById(R.id.spinnerSkill);
+        ArrayAdapter<CharSequence> skillAdapter = ArrayAdapter.createFromResource(this,
+                R.array.skillLevel_array, android.R.layout.simple_spinner_item);
+        skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSkill.setAdapter(skillAdapter);
+
+        profilePicture = (ImageView) findViewById(R.id.change_user_photo);
+        changeProfilePicture = (ImageButton) findViewById(R.id.change_user_photo);
+        changeProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent imgIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                imgIntent.setType("image/*");
+                startActivityForResult(imgIntent, 10);
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+//    @Override Sollte eigentlich funktionieren, aber wenn ausgewählt wird stürtzt es ab
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        // TODO Auto-generated method stub
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == Activity.RESULT_OK) {
+//            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+//            profilePicture.setImageBitmap(bitmap);
+//        }
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    // setContentView(R.layout.user_profile);
 }
+
