4af053a1cde4173af2b0ef57057f750a3c5ad0f6
moly94 <felix.mauler@gmail.com>
2017-01-26 19:58:34 +0100

    Fixed errors ChatActivity (DetailActivity from earlier projects), No Refactoring etc yet

--- app/src/main/java/bandm8s/hagenberg/fh/bandm8s/models/Chat.java
+++ app/src/main/java/bandm8s/hagenberg/fh/bandm8s/models/Chat.java
@@ -1,41 +1,65 @@
 package bandm8s.hagenberg.fh.bandm8s.models;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Felix on 24.01.2017.
  */
 
 public class Chat {
-    private String id;
-    private String text;
-    private String name;
+    public String uid;
+    public String author;
+    public String title;
+    public String description;
+    public boolean friendsOnly;
 
-    public String getName() {
-        return name;
+    public int starCount = 0;
+    public Map<String, Boolean> stars = new HashMap<>();
+    public ArrayList<String> tags = new ArrayList<>();
+
+    public Chat() {
+        // Default constructor required for calls to DataSnapshot.getValue(Story.class)
     }
 
-    public void setName(String name) {
-        this.name = name;
+
+    /**
+     * @param userId  the unique ID of the author of this story
+     * @param auth    Username of the author
+     * @param t       title of this story
+     * @param b       short description of the content
+     * @param tagList some metatags
+     * @param fOnly   is story public or friends-only?
+     */
+    public Chat(String userId, String auth, String t, String b, List<String> tagList, boolean fOnly) {
+        uid = userId;
+        author = auth;
+        title = t;
+        description = b;
+        tags = (ArrayList<String>) tagList;
+        friendsOnly = fOnly;
+
     }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public String getText() {
-        return text;
-    }
+    // [START post_to_map]
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", uid);
+        result.put("author", author);
+        result.put("title", title);
+        result.put("description", description);
+        result.put("starCount", starCount);
+        result.put("stars", stars);
+        result.put("friendsonly", friendsOnly);
 
-    public void setText(String text) {
-        this.text = text;
+        return result;
     }
-
-    public Chat(String id, String text, String name) {
-        this.id = id;
-        this.text = text;
-        this.name = name;
-    }
+    // [END post_to_map]
 }
