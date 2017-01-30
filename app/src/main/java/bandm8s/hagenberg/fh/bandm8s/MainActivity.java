eb354747e5cd4beb28d3f0fa723978a0fb07ae99
Felpower <s1510237003@students.fh-hagenebrg.at>
2017-01-30 17:31:55 +0100

    updated the main drawer, and deleted useless menus

--- app/src/main/java/bandm8s/hagenberg/fh/bandm8s/MainActivity.java
+++ app/src/main/java/bandm8s/hagenberg/fh/bandm8s/MainActivity.java
@@ -1,34 +1,28 @@
 package bandm8s.hagenberg.fh.bandm8s;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import bandm8s.hagenberg.fh.bandm8s.fragments.BandEntriesFragment;
-import bandm8s.hagenberg.fh.bandm8s.fragments.EntryListFragment;
 import bandm8s.hagenberg.fh.bandm8s.fragments.UserEntriesFragment;
 
 public class MainActivity extends BaseActivity
@@ -161,18 +155,19 @@ public class MainActivity extends BaseActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        }
+        if (id == R.id.nav_manage) {
             startActivity(new Intent(MainActivity.this, UserProfile.class));
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
         } else if (id == R.id.nav_logout) {
             logOutUser();
         }
