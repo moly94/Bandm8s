07540e59cea1ee6faa19b632bf2c9a77e29008ef
moly94 <felix.mauler@gmail.com>
2017-02-02 19:19:16 +0100

    Fixed Manifest

---
+++ app/src/main/java/bandm8s/hagenberg/fh/bandm8s/fragments/ChatEntriesPassiveFragment.java
@@ -0,0 +1,16 @@
+package bandm8s.hagenberg.fh.bandm8s.fragments;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+/**
+ * Created by Felix on 02.02.2017.
+ */
+
+public class ChatEntriesPassiveFragment extends EntryListFragment {
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        return databaseReference.child("user-chats-passive").child(getUid());
+    }
+}
